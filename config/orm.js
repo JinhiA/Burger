151a028dae06f2bb02cf11ba48c8ce9552e93fd1
Nalee Star <ahn.jinhi@gmail.com>
2020-09-23 21:57:39 -0400

    created burgerjs

--- config/orm.js
+++ config/orm.js
@@ -3,6 +3,7 @@ const connection = require('../config/connection.js');
 
 const orm = {
 
+  //mysql command to select all burgers
   selectAll: function (cb) {
     const queryString = "SELECT * FROM burgers";
     connection.query(queryString, function (err, result) {
@@ -13,6 +14,7 @@ const orm = {
     });
   },
 
+  //mysql command to insert burger
   insertOne: function (burger, cb) {
     const queryString = "INSERT INTO burgers (burger_name) VALUES (?)";
     connection.query(queryString, [burger], function (err, result) {
@@ -23,6 +25,7 @@ const orm = {
     });
   },
 
+  //mysql command to update burger
   updateOne: function (id, cb) {
    const queryString = "UPDATE burgers SET devoured = true WHERE id = ?";
 
@@ -35,4 +38,5 @@ const orm = {
   }
 };
 
+//Export the orm object for the model 
 module.exports = orm;
